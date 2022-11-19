9fb9292dc6292e8fd4e8d7926efa21db6af9be3c
ARUNMODONGAL <117196972+ARUNMODONGAL@users.noreply.github.com>
2022-11-19 07:45:03 +0530

    changed signup

--- script.js
+++ script.js
@@ -6,8 +6,27 @@ function verify(){
         alert("Fields cannot be empty");
         return false;
     }
+    else if(password.value.length<8){
+        alert("password length is insufficient");
+        return false;
+    }
     else{
         alert("form submitted");
         return true;
+    }
+}
+
+function validate(){
+    let regex=/^([A-Za-z0-9\.-#_]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+    if(regex.test(email.value)){
+        error.innerHTML="Email is valid";
+        error.style.color="green";
+        return true;
+
+    }
+    else{
+        error.innerHTML="Email is invalid";
+        error.style.color="red";
+        return false;
     }
 }
