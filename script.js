ddf889adc4bba5ddf8c4bb63b2e785a17be5ca05
ARUNMODONGAL <117196972+ARUNMODONGAL@users.noreply.github.com>
2022-11-19 19:52:33 +0530

    script added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var Username=document.getElementById("Username");
 var password=document.getElementById("pass");
+var phonenmumber=document.getElementById("mobile");
+
 
 function verify(){
     if((Username.value=="")||(password.value=="")){
@@ -17,6 +19,7 @@ function verify(){
 }
 
 function validate(){
+
     let regex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     if(regex.test(email.value)){
         error.innerHTML="Email is valid";
@@ -28,5 +31,39 @@ function validate(){
         error.innerHTML="Email is invalid";
         error.style.color="red";
         return false;
+    } 
     }
-}
+    function emailCheck(){
+
+        var email = document.getElementById('signEmail').value;
+        var emailHelpBlock = document.getElementById('emailHelpBlock');
+      
+        if(email.trim().length!=0){
+          if(!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)){
+            emailHelpBlock.innerHTML = 'Invalid email format!';
+            emailHelpBlock.style.color = 'Red';
+            return false;
+          }
+            emailHelpBlock.innerHTML = '';
+            return true;
+          }
+          emailHelpBlock.innerHTML = '';
+        }
+        function mobCheck(){
+
+            var mob = document.getElementById('mob').value;
+            var mobError = document.getElementById('mobHelpBlock');
+          
+            if(mob.trim().length!=0){
+              if(mob.match(/^(\d{3})[-. ]*(\d{3})[-. ]*(\d{4})$/)){
+                mobError.innerText = '';
+                return true;
+              }
+              else{
+                mobError.style.color = 'Red';
+                mobError.innerText = 'Invalid mobile number!';
+                return false;
+              }
+              }
+              mobError.innerText = '';
+            }      
